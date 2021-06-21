d382dc5e653f2b10eaa8da520ec41477633f9227
Giulio_Pa <giuliop93@hotmail.it>
2021-06-21 10:24:26 +0200

    somebody save me

--- laravel-comics/routes/web.php
+++ laravel-comics/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('layout');
-});
+})->name("pagina_home");
 
 Route::get('/', function () {
     return view('single');
